6a3aaef85d2f502409f77c2a4cb335d5b270b427
velcorn <iwishyouperish@gmail.com>
2022-06-11 13:17:23 +0200

    Exercise 5 initial commit

--- Exercise 3/exercise_3.py
+++ Exercise 3/exercise_3.py
@@ -20,8 +20,8 @@ for k in ['linear', 'poly', 'sigmoid', 'rbf']:
     ground_truth = [0] * 30 + [1] * 30
     features = []
     for img in images:
-        features.append([img[:, :, 0].min(), img[:, :, 1].min(), img[:, :, 2].min(),
-                         round(img[:, :, 0].mean()), round(img[:, :, 1].mean()), round(img[:, :, 2].mean())])
+        features.append([img[:, :, 0].min(), img[:, :, 1].min(), img[:, :, 2].min(), round(img[:, :, 0].mean()),
+                         round(img[:, :, 1].mean()), round(img[:, :, 2].mean())])
 
     # Split the data into training and testing sets, fit the model and print the results
     x_train, x_test, y_train, y_test = train_test_split(features, ground_truth, test_size=0.5, random_state=42)
